a84ab8b467d678fa42f0f94a3ddca3226ad37073
manmay2 <imanmay2@gmail.com>
2025-07-01 18:32:41 +0530

    Started with Deployment.

--- app.cjs
+++ app.cjs
@@ -13,19 +13,20 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")))
 const ExpressError = require("./utils/ExpressError.cjs");
 const User = require("./models/user.cjs");
-
+const MongoStore = require('connect-mongo');
 const listings_ = require("./routes/listing.cjs");
 const reviews_ = require("./routes/reviews.cjs");
 const user_ = require("./routes/userRouter.cjs");
 
 const session = require("express-session");
-const flash = require("connect-flash");
 
+const flash = require("connect-flash");
+const mongodb_url=process.env.MONGO_ATLAS;
 
 app.engine("ejs", ejsMate);
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(mongodb_url);
 }
 
 
@@ -40,9 +41,20 @@ app.listen(port, (req, res) => {
     console.log("Server is listening to : ", port);
 });
 
+const store=MongoStore.create({
+    mongoUrl:process.env.MONGO_ATLAS,
+    crypto:{
+        secret:"my-super_star"
+    },
+    touchAfter:24*3600
+});
 
+store.on(("error"),()=>{
+    console.log("Error in Mongo_Store");
+});
 
 let sessionObj = {
+    store:store,
     secret: "mysecretCode",
     resave: false,
     saveUninitialized: true
